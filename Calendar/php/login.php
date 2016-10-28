3d8285501dffb5d0cd44655a13d5f51fa4437cef
Steven <steemosan@gmail.com>
2016-10-28 14:44:01 -0400

    User login and beginnings of Cooke Session Marriage
    Session set on login, cookie set to id. Cookie retrieved on load to
    javascript.

--- Calendar/php/login.php
+++ Calendar/php/login.php
@@ -31,15 +31,41 @@ function user_login($u, $p){
   $res = $conn->query($sql);
   if ($res->num_rows > 0) {
     // rsult was found
-    $sql2 = "SELECT * FROM users WHERE username = '$u' AND password = '$p' LIMIT 1";
-    $res2 = $conn->query($sql2);
-    if ($res2->num_rows != 0) {
-      echo "Found combo";
+    $user_info = $res->fetch_assoc();
+    if (password_verify($p, $user_info["password"])) {
+      session_start();
+      $_SESSION["user"] = $u;
+      $_SESSION["pass"] = $p;
+      echo session_id();
     } else {
-      echo "Invalid Password";
+      echo "ERROR: Invalid Password";
     }
   } else {
-    echo "User not found...";
+    echo "ERROR: User not found...";
+  }
+}
+
+function signup($u, $p){
+  // References to the db login details
+  global $db_host, $db_user, $db_pass, $db_name;
+  // Connection object for the DB
+  $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+  // Ensure the connection was successful
+  if ($conn->connect_error) {
+    die("Unable to connect to databse: " . $conn->connect_error);
+  }
+  $chk_exist_sql = "SELECT * FROM users WHERE username = '$u' LIMIT 1";
+  $q_res = $conn->query($chk_exist_sql);
+  if ($q_res->num_rows > 0){
+    echo "ERROR: User Taken";
+  } else {
+      $p = password_hash($p, PASSWORD_DEFAULT);
+      $insert_sql = "INSERT INTO users (username, password) VALUES ('$u', '$p')";
+      if($conn->query($insert_sql) === TRUE){
+        echo "ERROR: Insert Successful!";
+      }else{
+        echo "ERROR: Unable to create user";
+      }
   }
 }
 
--- Calendar/scripts/main.js
+++ Calendar/scripts/main.js
@@ -1,3 +1,13 @@
+SESSION_COOKIE = null;
+
+function initialize() {
+  var cookie = getCookie();
+  if (cookie !== false){
+    SESSION_COOKIE = cookie.split('=')[1];
+  }
+  defCal();
+}
+
 function fetchCal() {
   // Function calls for the table to be made
 
@@ -61,14 +71,48 @@ function userLogin(){
       xhttp.onreadystatechange = function(){
         if (this.readyState == 4 && this.status == 200) {
           // If the request is successful, set the table to the response
-          document.getElementById("login_error").innerHTML = this.responseText;
-        }else {
-          echo this.readyState;
+          if(this.responseText.split(':')[0] == "ERROR"){
+            document.getElementById("login_error").innerHTML = this.responseText;
+          } else {
+            document.cookie = "sess_id=" + this.responseText
+          }
         }
       };
       // The request variables
       // Mode is create or login
       var pString = "user=" + username + "&pass=" + password + "&mode=" + "1";
+      // Open the connection with the server
+      xhttp.open("POST", "./php/login.php", true);
+      // Set a request header for POST
+      xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      // Send the request
+      xhttp.send(pString);
+    }
+}
+
+function userSignup(){
+  // Called when sign up is clicked. Attempts to create a user in with the given
+  // credentials
+  var username = document.getElementById("username").value,
+    password = document.getElementById("password").value;
+    if ( !valUser(username) || !valPass(password)){
+      document.getElementById("login_error").innerHTML =
+        "Invalid username or password. Spaces are forbidden";
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+    } else {
+      document.getElementById("login_error").innerHTML = "";
+      var xhttp = new XMLHttpRequest();
+      // add a function to call when the state changes successfully.
+      xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+          // If the request is successful, set the table to the response
+          document.getElementById("login_error").innerHTML = this.responseText;
+        }
+      };
+      // The request variables
+      // Mode is create or login
+      var pString = "user=" + username + "&pass=" + password + "&mode=" + "0";
       // Open the connection with the server
       xhttp.open("POST", "./php/login.php", true);
       // Set a request header for POST
@@ -87,4 +131,42 @@ function valYear(yr){
   else{
     return false;
   }
+}
+
+function getCookie(cookie){
+  var c_arr = document.cookie.split(';');
+  for (var i = 0; i < c_arr.length; i++) {
+    if(c_arr[i].split('=')[0] == cookie){
+      return c_arr[i];
+    }
+  }
+  return false;
+}
+
+function valUser(user){
+  var u = user.trim();
+  if (u.length < 3) {
+    return false;
+  }
+  return true;
+}
+
+function  valPass(pass){
+  var p = pass.trim(),
+  regex_caps = /[A-Z]/,
+  regex_lower = /[a-z]/,
+  regex_number = /[0-9]/;
+  if (p.indexOf(' ') > -1){
+    alert("password space");
+    return false;
+  }
+  if (p.length < 8){
+    alert("password Length");
+    return false;
+  }
+  if (!regex_number.test(p) || !regex_caps.test(p) || !regex_lower.test(p)){
+    alert("password reg");
+    return false;
+  }
+  return true;
 }
